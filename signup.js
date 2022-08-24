35bf9f0e025d37e7de054c416ff59f9d6ae7d1e7
rajeshranjan22 <107463246+rajeshranjan22@users.noreply.github.com>
2022-08-24 21:08:38 +0530

    add

---
+++ signup.js
@@ -0,0 +1,24 @@
+
+let arr=[];
+let signup=()=>{
+    event.preventDefault()
+    let firstname=document.getElementById("first").value;
+    let lastname=document.getElementById("last").value;
+    let email=document.getElementById("email").value;
+    let password=document.getElementById("psswrd").value;
+
+    let obj={
+        firstname:firstname,
+        lastname:lastname,
+        email:email,
+        password:password
+
+    }
+
+    arr.push(obj);
+    localStorage.setItem("data",JSON.stringify(arr))
+
+    console.log(arr)
+}
+
+
