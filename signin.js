02159374131044f45262b7121122ce68dde1b2ad
rajeshranjan22 <107463246+rajeshranjan22@users.noreply.github.com>
2022-08-25 20:56:01 +0530

    day-3

---
+++ signin.js
@@ -0,0 +1,29 @@
+let details=JSON.parse(localStorage.getItem("data"))
+
+            function signin(){
+
+           event.preventDefault()
+                let Email=document.getElementById("email").value;
+                let Password=document.getElementById("psswrd").value;
+
+              for(let i=0;i<details.length;i++){
+
+              if(Email==details[i].email && Password==details[i].password){
+                 
+                alert("SuccesFull Login")
+                // window.location="index.html"
+              } else{
+
+                alert("Try Again")
+           
+                  console.log(details[i].email)
+              }  
+              if(Email=="" || Password==""){
+
+                alert("Fill All Detail")
+                window.location="signup.html"
+              }
+
+              }
+
+            }
