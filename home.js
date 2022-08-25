50e46d2ffa923afb6b9e605be5b5e175902a4221
Chanchal Soni <chanchalsoni1998@gmail.com>
2022-08-25 16:18:41 +0530

    display

---
+++ home.js
@@ -0,0 +1,23 @@
+let imagelink = [
+    "img1.webp", "img2.webp", "img3.webp","img4.webp","img5.webp","img6.webp", "img7.webp", "img8.webp"
+  ]
+  let leftbtn = document.getElementById("left");
+  let rightbtn = document.getElementById("right");
+  let slider = document.getElementById("slider");
+
+  let imagestate = 0;
+  rightbtn.addEventListener("click", function () {
+    imagestate++;
+    if(imagestate===imagelink.length){
+      imagestate=0;
+    }
+    slider.src = imagelink[imagestate];
+  })
+
+  leftbtn.addEventListener("click", function () {
+    imagestate--;
+    if(imagestate<0){
+      imagestate=imagelink.length-1;
+    }
+    slider.src = imagelink[imagestate];
+  })
