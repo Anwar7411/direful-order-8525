7410c2f67642e1955643a5ee9bbb00f17394b250
Chanchal Soni <chanchalsoni1998@gmail.com>
2022-08-27 12:12:37 +0530

    day-5

--- home.js
+++ home.js
@@ -20,6 +20,30 @@ let imagelink = [
       imagestate=imagelink.length-1;
     }
     slider.src = imagelink[imagestate];
+  });
+
+  let imagelink1 = [
+    "img1.webp", "img2.webp", "img3.webp","img4.webp","img5.webp","img6.webp", "img7.webp", "img8.webp"
+  ]
+  let leftbtn1 = document.getElementById("left1");
+  let rightbtn1 = document.getElementById("right1");
+  let slider1 = document.getElementById("slider1");
+
+  let imagestate1 = 0;
+  rightbtn1.addEventListener("click", function () {
+    imagestate1++;
+    if(imagestate1===imagelink1.length){
+      imagestate1=0;
+    }
+    slider1.src = imagelink1[imagestate1];
+  })
+
+  leftbtn1.addEventListener("click", function () {
+    imagestate1--;
+    if(imagestate1<0){
+      imagestate1=imagelink1.length-1;
+    }
+    slider1.src = imagelink1[imagestate1];
   })
 
   
@@ -30,6 +54,15 @@ let imagelink = [
     slider.src = imagelink[imagestate];
    imagestate++;
    
+
+},2000);
+setInterval(function(){   
+  if(imagestate1===imagelink1.length){
+    imagestate1 = 0;                
+  } 
+  slider1.src = imagelink1[imagestate1];
+ imagestate1++;
+ 
 
 },2000);
 function view_arrival(){
